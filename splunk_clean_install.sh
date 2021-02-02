d542a9dc04d94e835f0903f449d578e4d58e5122
William Pratt <billp@billpratt.net>
2021-02-02 10:08:15 -0600

    Update url / filename to 8.1.2

--- splunk_clean_install.sh
+++ splunk_clean_install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-WGET_URL="https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=8.1.1&product=splunk&filename=splunk-8.1.1-08187535c166-Linux-x86_64.tgz&wget=true"
-FILENAME="splunk-8.1.1-08187535c166-Linux-x86_64.tgz"
+WGET_URL="https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=8.1.2&product=splunk&filename=splunk-8.1.2-545206cc9f70-Linux-x86_64.tgz&wget=true"
+FILENAME="splunk-8.1.2-545206cc9f70-Linux-x86_64.tgz"
 DL_PATH="/tmp/$FILENAME"
 INSTALL_DIR="/opt"
 SPLUNK_HOME="$INSTALL_DIR/splunk"
