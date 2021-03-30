4ee465c1e3e17759116ed1094125e8e92f5a33b9
William Pratt <billp@billpratt.net>
2021-03-30 09:54:11 -0500

    Update splunkforwarder_clean_install.sh
    Update url / filename for 8.1.3

--- splunkforwarder_clean_install.sh
+++ splunkforwarder_clean_install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-WGET_URL="https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=8.1.2&product=universalforwarder&filename=splunkforwarder-8.1.2-545206cc9f70-Linux-x86_64.tgz&wget=true"
-FILENAME="splunkforwarder-8.1.2-545206cc9f70-Linux-x86_64.tgz"
+WGET_URL="https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=8.1.3&product=universalforwarder&filename=splunkforwarder-8.1.3-63079c59e632-Linux-x86_64.tgz&wget=true"
+FILENAME="splunkforwarder-8.1.3-63079c59e632-Linux-x86_64.tgz"
 DL_PATH="/tmp/$FILENAME"
 INSTALL_DIR="/opt"
 SPLUNK_HOME="$INSTALL_DIR/splunkforwarder"
